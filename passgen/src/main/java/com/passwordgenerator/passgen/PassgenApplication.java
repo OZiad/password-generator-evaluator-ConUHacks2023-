b7823d9cd7d2f3950883f627d05be76ff1f6f6f8
Omar Ziad <omar.ziad1@outlook.com>
2023-01-21 22:13:29 -0500

    Create REST api

--- passgen/src/main/java/com/passwordgenerator/passgen/PassgenApplication.java
+++ passgen/src/main/java/com/passwordgenerator/passgen/PassgenApplication.java
@@ -4,17 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @SpringBootApplication
-@RestController
 public class PassgenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassgenApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello(){
-		return "Hello World!";
 	}
 }
